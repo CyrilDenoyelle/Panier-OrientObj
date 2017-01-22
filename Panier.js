5bc1c7f6515b8d3dabdba47e5057df300205bd5b
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-01-22 23:52:58 +0100

    ajout d'un html

--- Panier.js
+++ Panier.js
@@ -1,32 +1,33 @@
-var Produit = function(nom, prix){
-	this.nom = nom;
-	this.prix = prix;
-};
+(function(){
+	var Produit = function(nom, prix){
+		this.nom = nom;
+		this.prix = prix;
+	};
 
-var Vienoiserie = function(frais){
+	// var Vienoiserie = function(frais){
+	// };
 	
-};
-var Panier = function(){
-	this.totalHT = 0;
-	this.ajoute = function(objet, nbr){
-		if(nbr ===undefined){
-			this.totalHT = this.totalHT + objet.prix;
-		}else{
-			this.totalHT = this.totalHT + (objet.prix*nbr);
-		}
-		this.totalHT = Math.floor(this.totalHT*100)/100;
-		this.totalTTC = Math.ceil((this.totalHT * 1.20)*100)/100;
+	var Panier = function(){
+		this.totalHT = 0;
+		this.ajoute = function(objet, nbr){
+			if(nbr ===undefined){
+				this.totalHT = this.totalHT + objet.prix;
+			}else{
+				this.totalHT = this.totalHT + (objet.prix*nbr);
+			}
+			this.totalHT = Math.floor(this.totalHT*100)/100;
+			this.totalTTC = Math.ceil((this.totalHT * 1.20)*100)/100;
+		};
+		this.retire = function(objet, nbr){
+			if(nbr === undefined){
+				this.totalHT = this.totalHT - objet.prix;
+			}else{
+				this.totalHT = this.totalHT - (objet.prix*nbr);
+			}
+			this.totalHT = Math.floor(this.totalHT*100)/100;
+			this.totalTTC = Math.floor((this.totalHT * 1.20)*100)/100;
+		};
 	};
-	this.retire = function(objet, nbr){
-		if(nbr === undefined){
-			this.totalHT = this.totalHT - objet.prix;
-		}else{
-			this.totalHT = this.totalHT - (objet.prix*nbr);
-		}
-		this.totalHT = Math.floor(this.totalHT*100)/100;
-		this.totalTTC = Math.floor((this.totalHT * 1.20)*100)/100;
-	};
-};
 //console.log(typeof baguette == 'object');
 var baguette = new Produit( 'Baguette', 0.85); // prix HT
 var croissant = new Produit( 'Croissant', 0.80);
@@ -41,3 +42,4 @@ panier.ajoute(croissant, 2);
 
 console.log(panier.totalHT);
 console.log(panier.totalTTC);
+})();
